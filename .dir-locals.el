967c1363781a494b80fe7350bac36839f2d1e4e0
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-11-07 18:15:16 -0500

    chore: set dir-local python-indent-offset (Emacs)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(("./" . ((compile-command . "python3 main.py"))))
+(("./" . ((python-indent-offset . 4)
+          (compile-command . "python3 main.py"))))
