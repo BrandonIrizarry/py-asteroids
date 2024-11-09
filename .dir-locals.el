d71896fd78401f00a30a705120eb2437da72a449
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-11-08 19:01:05 -0500

    chore: specify python-mode for compile-command (Emacs)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-(("./" . ((python-indent-offset . 4)
-          (compile-command . "python3 main.py"))))
+((python-mode . ((python-indent-offset . 4)
+                 (compile-command . "python3 main.py"))))
